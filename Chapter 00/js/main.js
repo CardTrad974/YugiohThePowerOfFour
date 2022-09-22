e361e0cb04467123e568eeedd174a5716e117541
CardTrad974 <dyuerisutox974@gmail.com>
2022-09-22 18:29:40 +0400

    ex

--- Chapter 00/js/main.js
+++ Chapter 00/js/main.js
@@ -1,2 +1,2 @@
-document.querySelector("audio1").autoplay = false;
-document.querySelector("audio2").autoplay = false;
+// document.querySelector("audio1").autoplay = false;
+// document.querySelector("audio2").autoplay = false;
