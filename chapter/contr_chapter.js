504cf787a66e6098d3e7ad2a25c5a89bad7394f0
CardTrad974 <dyuerisutox974@gmail.com>
2022-10-29 22:21:17 +0400

    add new chapter + new name file/folder

--- chapter/contr_chapter.js
+++ chapter/contr_chapter.js
@@ -1,25 +1,27 @@
 var chapter = document.getElementById('chap_select_debut').innerHTML = 
 "<option value=\"\">Select...</option>"+
-"<option value=\"../Chapter 00/prologue.html\">00. Prologue</option>"+
-"<option value=\"../Chapter 01/Chapitre 1.html\">01. XYZ</option>"+
-"<option value=\"../Chapter 02/Chapitre 2.html\">02. SYNCHRO</option>"+
-"<option value=\"../Chapter 03/Chapitre 3.html\">03. FUSION - PART 1</option>"+
-"<option value=\"../Chapter 04/Chapitre 4.html\">04. FUSION - PART 2</option>"+
-"<option value=\"../Chapter 05/Chapitre 5.html\">05. STANDARD - PART 1</option>"+
-"<option value=\"../Chapter 06/Chapitre 6.html\">06. STANDARD - PART 2</option>"+
-"<option value=\"../Chapter 07/Chapitre 7.html\">07. STANDARD - PART 3</option>"+
-"<option value=\"../Chapter 08/Chapitre 8.html\">08. ZEXAL - PART 1</option>"+
-"<option value=\"../Chapter 09/Chapitre 9.html\">09. ZEXAL - PART 2</option>";
+"<option value=\"../Chapter_00/prologue.html\">00. Prologue</option>"+
+"<option value=\"../Chapter_01/Chapitre_1.html\">01. XYZ</option>"+
+"<option value=\"../Chapter_02/Chapitre_2.html\">02. SYNCHRO</option>"+
+"<option value=\"../Chapter_03/Chapitre_3.html\">03. FUSION - PART 1</option>"+
+"<option value=\"../Chapter_04/Chapitre_4.html\">04. FUSION - PART 2</option>"+
+"<option value=\"../Chapter_05/Chapitre_5.html\">05. STANDARD - PART 1</option>"+
+"<option value=\"../Chapter_06/Chapitre_6.html\">06. STANDARD - PART 2</option>"+
+"<option value=\"../Chapter_07/Chapitre_7.html\">07. STANDARD - PART 3</option>"+
+"<option value=\"../Chapter_08/Chapitre_8.html\">08. ZEXAL - PART 1</option>"+
+"<option value=\"../Chapter_09/Chapitre_9.html\">09. ZEXAL - PART 2</option>"+
+"<option value=\"../Chapter_10/Chapitre_10.html\">10. SYNCHRO - PART 1</option>";
 
 var chapter2 = document.getElementById('chap_select_fin').innerHTML = 
 "<option value=\"\">Select...</option>"+
-"<option value=\"../Chapter 00/prologue.html\">00. Prologue</option>"+
-"<option value=\"../Chapter 01/Chapitre 1.html\">01. XYZ</option>"+
-"<option value=\"../Chapter 02/Chapitre 2.html\">02. SYNCHRO</option>"+
-"<option value=\"../Chapter 03/Chapitre 3.html\">03. FUSION - PART 1</option>"+
-"<option value=\"../Chapter 04/Chapitre 4.html\">04. FUSION - PART 2</option>"+
-"<option value=\"../Chapter 05/Chapitre 5.html\">05. STANDARD - PART 1</option>"+
-"<option value=\"../Chapter 06/Chapitre 6.html\">06. STANDARD - PART 2</option>"+
-"<option value=\"../Chapter 07/Chapitre 7.html\">07. STANDARD - PART 3</option>"+
-"<option value=\"../Chapter 08/Chapitre 8.html\">08. ZEXAL - PART 1</option>"+
-"<option value=\"../Chapter 09/Chapitre 9.html\">09. ZEXAL - PART 2</option>";
+"<option value=\"../Chapter_00/prologue.html\">00. Prologue</option>"+
+"<option value=\"../Chapter_01/Chapitre_1.html\">01. XYZ</option>"+
+"<option value=\"../Chapter_02/Chapitre_2.html\">02. SYNCHRO</option>"+
+"<option value=\"../Chapter_03/Chapitre_3.html\">03. FUSION - PART 1</option>"+
+"<option value=\"../Chapter_04/Chapitre_4.html\">04. FUSION - PART 2</option>"+
+"<option value=\"../Chapter_05/Chapitre_5.html\">05. STANDARD - PART 1</option>"+
+"<option value=\"../Chapter_06/Chapitre_6.html\">06. STANDARD - PART 2</option>"+
+"<option value=\"../Chapter_07/Chapitre_7.html\">07. STANDARD - PART 3</option>"+
+"<option value=\"../Chapter_08/Chapitre_8.html\">08. ZEXAL - PART 1</option>"+
+"<option value=\"../Chapter_09/Chapitre_9.html\">09. ZEXAL - PART 2</option>"+
+"<option value=\"../Chapter_10/Chapitre_10.html\">10. SYNCHRO - PART 1</option>";
