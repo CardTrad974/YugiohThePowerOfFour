7a0f5ebf646661948919dbc3f6ea0b86f8162119
CardTrad974 <dyuerisutox974@gmail.com>
2022-11-04 09:36:53 +0400

    correct

--- chapter/contr_chapter.js
+++ chapter/contr_chapter.js
@@ -10,7 +10,8 @@ var chapter = document.getElementById('chap_select_debut').innerHTML =
 "<option value=\"../Chapter_07/Chapitre_7.html\">07. STANDARD - PART 3</option>"+
 "<option value=\"../Chapter_08/Chapitre_8.html\">08. ZEXAL - PART 1</option>"+
 "<option value=\"../Chapter_09/Chapitre_9.html\">09. ZEXAL - PART 2</option>"+
-"<option value=\"../Chapter_10/Chapitre_10.html\">10. SYNCHRO - PART 1</option>";
+"<option value=\"../Chapter_10/Chapitre_10.html\">10. 5D's - PART 1</option>"+
+"<option value=\"../Chapter_11/Chapitre_11.html\">11. 5D's - PART 2</option>";
 
 var chapter2 = document.getElementById('chap_select_fin').innerHTML = 
 "<option value=\"\">Select...</option>"+
@@ -24,4 +25,5 @@ var chapter2 = document.getElementById('chap_select_fin').innerHTML =
 "<option value=\"../Chapter_07/Chapitre_7.html\">07. STANDARD - PART 3</option>"+
 "<option value=\"../Chapter_08/Chapitre_8.html\">08. ZEXAL - PART 1</option>"+
 "<option value=\"../Chapter_09/Chapitre_9.html\">09. ZEXAL - PART 2</option>"+
-"<option value=\"../Chapter_10/Chapitre_10.html\">10. SYNCHRO - PART 1</option>";
+"<option value=\"../Chapter_10/Chapitre_10.html\">10. 5D's - PART 1</option>"+
+"<option value=\"../Chapter_11/Chapitre_11.html\">11. 5D's - PART 2</option>";
