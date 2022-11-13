6cbfda543b4735b5f25fb0040153aa0d7fb81df0
CardTrad974 <dyuerisutox974@gmail.com>
2022-11-13 17:45:47 +0400

    new chapter

---
+++ Chapter_12/js/audio.js
@@ -0,0 +1,480 @@
+var audioContr1 = document.getElementById("audioPlayer");
+var center1 = document.getElementById("zone1");
+
+audioContr1.onplay = function () {
+
+    if (audioContr1.play){
+        
+        center1.classList.add("audioFixe");
+        audioContr1.loop = true;
+
+    }
+
+
+}
+
+audioContr1.onpause = function () {
+            
+    if(audioContr1.pause){
+
+        center1.classList.remove("audioFixe");
+        audioContr1.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr2 = document.getElementById("audioPlayer2");
+var center2 = document.getElementById("zone2");
+
+audioContr2.onplay = function () {
+
+    if (audioContr2.play){
+        
+        center2.classList.add("audioFixe");
+        audioContr2.loop = true;
+
+    }
+
+
+}
+
+audioContr2.onpause = function () {
+            
+    if(audioContr2.pause){
+
+        center2.classList.remove("audioFixe");
+        audioContr2.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr3 = document.getElementById("audioPlayer3");
+var center3 = document.getElementById("zone3");
+
+audioContr3.onplay = function () {
+
+    if (audioContr3.play){
+        
+        center3.classList.add("audioFixe");
+        audioContr3.loop = true;
+
+    }
+
+
+}
+
+audioContr3.onpause = function () {
+            
+    if(audioContr3.pause){
+
+        center3.classList.remove("audioFixe");
+        audioContr3.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr4 = document.getElementById("audioPlayer4");
+var center4 = document.getElementById("zone4");
+
+audioContr4.onplay = function () {
+
+    if (audioContr4.play){
+        
+        center4.classList.add("audioFixe");
+        audioContr4.loop = true;
+
+    }
+
+
+}
+
+audioContr4.onpause = function () {
+            
+    if(audioContr4.pause){
+
+        center4.classList.remove("audioFixe");
+        audioContr4.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr5 = document.getElementById("audioPlayer5");
+var center5 = document.getElementById("zone5");
+
+audioContr5.onplay = function () {
+
+    if (audioContr5.play){
+        
+        center5.classList.add("audioFixe");
+        audioContr5.loop = true;
+
+    }
+
+
+}
+
+audioContr5.onpause = function () {
+            
+    if(audioContr5.pause){
+
+        center5.classList.remove("audioFixe");
+        audioContr5.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr6 = document.getElementById("audioPlayer6");
+var center6 = document.getElementById("zone6");
+
+audioContr6.onplay = function () {
+
+    if (audioContr6.play){
+        
+        center6.classList.add("audioFixe");
+        audioContr6.loop = true;
+
+    }
+
+
+}
+
+audioContr6.onpause = function () {
+            
+    if(audioContr6.pause){
+
+        center6.classList.remove("audioFixe");
+        audioContr6.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr7 = document.getElementById("audioPlayer7");
+var center7 = document.getElementById("zone7");
+
+audioContr7.onplay = function () {
+
+    if (audioContr7.play){
+        
+        center7.classList.add("audioFixe");
+        audioContr7.loop = true;
+
+    }
+
+
+}
+
+audioContr7.onpause = function () {
+            
+    if(audioContr7.pause){
+
+        center7.classList.remove("audioFixe");
+        audioContr7.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr8 = document.getElementById("audioPlayer8");
+var center8 = document.getElementById("zone8");
+
+audioContr8.onplay = function () {
+
+    if (audioContr8.play){
+        
+        center8.classList.add("audioFixe");
+        audioContr8.loop = true;
+
+    }
+
+
+}
+
+audioContr8.onpause = function () {
+            
+    if(audioContr8.pause){
+
+        center8.classList.remove("audioFixe");
+        audioContr8.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr9 = document.getElementById("audioPlayer9");
+var center9 = document.getElementById("zone9");
+
+audioContr9.onplay = function () {
+
+    if (audioContr9.play){
+        
+        center9.classList.add("audioFixe");
+        audioContr9.loop = true;
+
+    }
+
+
+}
+
+audioContr9.onpause = function () {
+            
+    if(audioContr9.pause){
+
+        center9.classList.remove("audioFixe");
+        audioContr9.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr10 = document.getElementById("audioPlayer10");
+var center10 = document.getElementById("zone10");
+
+audioContr10.onplay = function () {
+
+    if (audioContr10.play){
+        
+        center10.classList.add("audioFixe");
+        audioContr10.loop = true;
+
+    }
+
+
+}
+
+audioContr10.onpause = function () {
+            
+    if(audioContr10.pause){
+
+        center10.classList.remove("audioFixe");
+        audioContr10.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr11 = document.getElementById("audioPlayer11");
+var center11 = document.getElementById("zone11");
+
+audioContr11.onplay = function () {
+
+    if (audioContr11.play){
+        
+        center11.classList.add("audioFixe");
+        audioContr11.loop = true;
+
+    }
+
+
+}
+
+audioContr11.onpause = function () {
+            
+    if(audioContr11.pause){
+
+        center11.classList.remove("audioFixe");
+        audioContr11.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr12 = document.getElementById("audioPlayer12");
+var center12 = document.getElementById("zone12");
+
+audioContr12.onplay = function () {
+
+    if (audioContr12.play){
+        
+        center12.classList.add("audioFixe");
+        audioContr12.loop = true;
+
+    }
+
+
+}
+
+audioContr12.onpause = function () {
+            
+    if(audioContr12.pause){
+
+        center12.classList.remove("audioFixe");
+        audioContr12.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr13 = document.getElementById("audioPlayer13");
+var center13 = document.getElementById("zone13");
+
+audioContr13.onplay = function () {
+
+    if (audioContr13.play){
+        
+        center13.classList.add("audioFixe");
+        audioContr13.loop = true;
+
+    }
+
+
+}
+
+audioContr13.onpause = function () {
+            
+    if(audioContr13.pause){
+
+        center13.classList.remove("audioFixe");
+        audioContr13.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr14 = document.getElementById("audioPlayer14");
+var center14 = document.getElementById("zone14");
+
+audioContr14.onplay = function () {
+
+    if (audioContr14.play){
+        
+        center14.classList.add("audioFixe");
+        audioContr14.loop = true;
+
+    }
+
+
+}
+
+audioContr14.onpause = function () {
+            
+    if(audioContr14.pause){
+
+        center14.classList.remove("audioFixe");
+        audioContr14.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr15 = document.getElementById("audioPlayer15");
+var center15 = document.getElementById("zone15");
+
+audioContr15.onplay = function () {
+
+    if (audioContr15.play){
+        
+        center15.classList.add("audioFixe");
+        audioContr15.loop = true;
+
+    }
+
+
+}
+
+audioContr15.onpause = function () {
+            
+    if(audioContr15.pause){
+
+        center15.classList.remove("audioFixe");
+        audioContr15.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr16 = document.getElementById("audioPlayer16");
+var center16 = document.getElementById("zone16");
+
+audioContr16.onplay = function () {
+
+    if (audioContr16.play){
+        
+        center16.classList.add("audioFixe");
+        audioContr16.loop = true;
+
+    }
+
+}
+
+audioContr16.onpause = function () {
+            
+    if(audioContr16.pause){
+
+        center16.classList.remove("audioFixe");
+        audioContr16.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr17 = document.getElementById("audioPlayer17");
+var center17 = document.getElementById("zone17");
+
+audioContr17.onplay = function () {
+
+    if (audioContr17.play){
+        
+        center17.classList.add("audioFixe");
+        audioContr17.loop = true;
+
+    }
+
+}
+
+audioContr17.onpause = function () {
+            
+    if(audioContr17.pause){
+
+        center17.classList.remove("audioFixe");
+        audioContr17.loop = false;
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var audioContr18 = document.getElementById("audioPlayer18");
+var center18 = document.getElementById("zone18");
+
+audioContr18.onplay = function () {
+
+    if (audioContr18.play){
+        
+        center18.classList.add("audioFixe");
+        audioContr18.loop = true;
+
+    }
+
+}
+
+audioContr18.onpause = function () {
+            
+    if(audioContr18.pause){
+
+        center18.classList.remove("audioFixe");
+        audioContr18.loop = false;
+    }
+
+}
--- chapter/contr_chapter.js
+++ chapter/contr_chapter.js
@@ -11,7 +11,8 @@ var chapter = document.getElementById('chap_select_debut').innerHTML =
 "<option value=\"../Chapter_08/Chapitre_8.html\">08. ZEXAL - PART 1</option>"+
 "<option value=\"../Chapter_09/Chapitre_9.html\">09. ZEXAL - PART 2</option>"+
 "<option value=\"../Chapter_10/Chapitre_10.html\">10. 5D's - PART 1</option>"+
-"<option value=\"../Chapter_11/Chapitre_11.html\">11. 5D's - PART 2</option>";
+"<option value=\"../Chapter_11/Chapitre_11.html\">11. 5D's - PART 2</option>"+
+"<option value=\"../Chapter_12/Chapitre_12.html\">12. GX - PART 1</option>";
 
 var chapter2 = document.getElementById('chap_select_fin').innerHTML = 
 "<option value=\"\">Select...</option>"+
@@ -26,4 +27,5 @@ var chapter2 = document.getElementById('chap_select_fin').innerHTML =
 "<option value=\"../Chapter_08/Chapitre_8.html\">08. ZEXAL - PART 1</option>"+
 "<option value=\"../Chapter_09/Chapitre_9.html\">09. ZEXAL - PART 2</option>"+
 "<option value=\"../Chapter_10/Chapitre_10.html\">10. 5D's - PART 1</option>"+
-"<option value=\"../Chapter_11/Chapitre_11.html\">11. 5D's - PART 2</option>";
+"<option value=\"../Chapter_11/Chapitre_11.html\">11. 5D's - PART 2</option>"+
+"<option value=\"../Chapter_12/Chapitre_12.html\">12. GX - PART 1</option>";
