c2d78e344b15fedc5827acccfd7b8242a8870a49
CardTrad974 <dyuerisutox974@gmail.com>
2022-11-23 17:19:06 +0400

    add new chapter

--- chapter/contr_chapter.js
+++ chapter/contr_chapter.js
@@ -12,7 +12,8 @@ var chapter = document.getElementById('chap_select_debut').innerHTML =
 "<option value=\"../Chapter_09/Chapitre_9.html\">09. ZEXAL - PART 2</option>"+
 "<option value=\"../Chapter_10/Chapitre_10.html\">10. 5D's - PART 1</option>"+
 "<option value=\"../Chapter_11/Chapitre_11.html\">11. 5D's - PART 2</option>"+
-"<option value=\"../Chapter_12/Chapitre_12.html\">12. GX - PART 1</option>";
+"<option value=\"../Chapter_12/Chapitre_12.html\">12. GX - PART 1</option>"+
+"<option value=\"../Chapter_13/Chapitre_13.html\">13. GX - PART 2</option>";
 
 var chapter2 = document.getElementById('chap_select_fin').innerHTML = 
 "<option value=\"\">Select...</option>"+
@@ -28,4 +29,5 @@ var chapter2 = document.getElementById('chap_select_fin').innerHTML =
 "<option value=\"../Chapter_09/Chapitre_9.html\">09. ZEXAL - PART 2</option>"+
 "<option value=\"../Chapter_10/Chapitre_10.html\">10. 5D's - PART 1</option>"+
 "<option value=\"../Chapter_11/Chapitre_11.html\">11. 5D's - PART 2</option>"+
-"<option value=\"../Chapter_12/Chapitre_12.html\">12. GX - PART 1</option>";
+"<option value=\"../Chapter_12/Chapitre_12.html\">12. GX - PART 1</option>"+
+"<option value=\"../Chapter_13/Chapitre_13.html\">13. GX - PART 2</option>";
