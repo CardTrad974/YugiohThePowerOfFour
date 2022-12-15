c0886b7ef2f1c3ce148157a65f46a8491266e75e
CardTrad974 <dyuerisutox974@gmail.com>
2022-12-15 23:21:22 +0400

    correct chapter

--- chapter/contr_chapter_eng.js
+++ chapter/contr_chapter_eng.js
@@ -3,7 +3,7 @@ var chapter = document.getElementById('chap_select_debut').innerHTML =
 "<option value=\"../Chapter_00/prologue_eng.html\">00. Prologue</option>"+
 "<option value=\"../Chapter_01/Chapter_1.html\">01. XYZ</option>"+
 "<option value=\"../Chapter_02/Chapter_2.html\">02. SYNCHRO</option>"+
-"<option value=\"../Chapter_03/Chapitre_3.html\">03. FUSION - PART 1</option>";
+"<option value=\"../Chapter_03/Chapter_3.html\">03. FUSION - PART 1</option>";
 // "<option value=\"../Chapter_04/Chapitre_4.html\">04. FUSION - PART 2</option>"+
 // "<option value=\"../Chapter_05/Chapitre_5.html\">05. STANDARD - PART 1</option>"+
 // "<option value=\"../Chapter_06/Chapitre_6.html\">06. STANDARD - PART 2</option>"+
@@ -18,7 +18,7 @@ var chapter2 = document.getElementById('chap_select_fin').innerHTML =
 "<option value=\"../Chapter_00/prologue_eng.html\">00. Prologue</option>"+
 "<option value=\"../Chapter_01/Chapter_1.html\">01. XYZ</option>"+
 "<option value=\"../Chapter_02/Chapter_2.html\">02. SYNCHRO</option>"+
-"<option value=\"../Chapter_03/Chapitre_3.html\">03. FUSION - PART 1</option>";
+"<option value=\"../Chapter_03/Chapter_3.html\">03. FUSION - PART 1</option>";
 // "<option value=\"../Chapter_04/Chapitre_4.html\">04. FUSION - PART 2</option>"+
 // "<option value=\"../Chapter_05/Chapitre_5.html\">05. STANDARD - PART 1</option>"+
 // "<option value=\"../Chapter_06/Chapitre_6.html\">06. STANDARD - PART 2</option>"+
